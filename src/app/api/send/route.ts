e7a4b214d39e51b5cf1acebc86224949a9d4e398
amr-radwan1 <amrradwan2006@gmail.com>
2024-10-30 21:22:41 -0400

    updates

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,23 +1,25 @@
 import { EmailTemplate } from '../../../components/EmailTemplate';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST() {
-  try {
-    const { data, error } = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['amrradwan2006@gmail.com'],
-      subject: 'Hello world',
-      react: EmailTemplate({ firstName: 'John' }),
-    });
+export const dynamic = "force-dynamic";
 
-    if (error) {
-      return Response.json({ error }, { status: 500 });
+export async function GET() {
+    const resend = new Resend(process.env.RESEND_API_KEY);
+    try {
+        const { data, error } = await resend.emails.send({
+        from: 'Acme <onboarding@resend.dev>',
+        to: ['amrradwan2006@gmail.com'],
+        subject: 'Hello world',
+        react: EmailTemplate({ firstName: 'Amr' }),
+        });
+
+        if (error) {
+        return Response.json({ error }, { status: 500 });
+        }
+
+        return Response.json(data);
+    } catch (error) {
+        return Response.json({ error }, { status: 500 });
     }
-
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
-  }
 }
