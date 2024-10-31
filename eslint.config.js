69dc3f0d5d5b64b6b170d8cb9f86ee817f757246
amr-radwan1 <amrradwan2006@gmail.com>
2024-10-30 22:07:18 -0400

    deploy

--- eslint.config.js
+++ eslint.config.js
@@ -34,5 +34,6 @@ export default [
         { allowConstantExport: true },
       ],
     },
+    "extends": ["eslint:recommended", "next"],
   },
 ]
--- src/components/EmailTemplate.tsx
+++ src/components/EmailTemplate.tsx
@@ -2,15 +2,16 @@ import * as React from 'react';
 
 interface EmailTemplateProps {
   firstName: string;
-  message: string; // Add message prop
+  message: string;
 }
 
 export const EmailTemplate: React.FC<Readonly<EmailTemplateProps>> = ({
   firstName,
-  message, // Destructure the message prop
+  message,
 }) => (
   <div>
     <h1>Message from, {firstName}!</h1>
+    <h1>Phone Number: </h1>
     <p>{message}</p> {/* Display the message */}
   </div>
 );
