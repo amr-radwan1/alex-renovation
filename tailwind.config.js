6b0d3ef2f42a276f3e0027ff6b412c62a1a361f7
amr-radwan1 <amrradwan2006@gmail.com>
2024-11-02 19:23:34 -0400

    bigger screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,22 @@ export default {
           'tailoredOrange':'#d84315'
         }
     },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1921px',
+      // => @media (min-width: 1921px) { ... }
+    }
   },
   plugins: [],
 }
